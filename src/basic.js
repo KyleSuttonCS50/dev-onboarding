8faad44e4e48f80c779986d465771ca7693bf4a0
Kyle Sutton <kylesutton@Kathleens-MacBook-Pro.local>
2018-04-29 13:26:40 -0400

    week 3?

--- src/basic.js
+++ src/basic.js
@@ -15,6 +15,9 @@ class Basic extends Component {
         <h3> Praise for Kyle Sutton: </h3>
         <p className="body"> "Also you are so goddamn smart 10/10 go Kyle would recommend" -- Olivia Graham </p>
         <p className="body"> "Could be granola gay" -- Also Olivia Graham </p>
+        <p className="body"> "Looks a little unrefined but has a heart of gold" -- Lauren Toman </p>
+        <p className="body"> "If I was lost in the woods, he would either save me or murder me" -- Randle Steinbeck </p>
+        <p className="body"> "Doesn't look as much like Post Malone as I expected" -- Caroline Graham </p>
         <h3> Check out some other stuff about me: </h3>
         <p className="icons-body"> 
           <a className="link" href="https://www.linkedin.com/in/kyle-sutton-0a7008158/" target="_"> <i class="fab fa-linkedin"></i> </a>
